436cab430df69b58fc1198e68556d82f1e3c755d
Edokpayi Rejoice <rejoiceoedokpayi@gmail.com>
2022-06-17 15:46:33 +0100

    Rejoice Profile

---
+++ app.js
@@ -0,0 +1,20 @@
+const { createServer } = require('http');
+var fs = require('fs');
+var url = require('url');
+
+const server = createServer((req, res) => {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, (err, data) => {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end('404 Not Found');
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end();
+        }
+    });
+});
+
+server.listen(8080);
